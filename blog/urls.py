a558f73cc1904d5dbd08fad5e451201245edc9b9
Illia Smyhunov <illia.smyhunov@jetsoftpro.com>
2023-02-11 18:59:36 +0200

    Add css for all sections and make views return the templates

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("posts/", views.posts, name="posts-page"),
-    path("posts/<slug:post>/", views.post_detail,
+    path("posts/<slug:slug>/", views.post_detail,
          name="post-detail-page")  # /posts/my-first-post
 ]
